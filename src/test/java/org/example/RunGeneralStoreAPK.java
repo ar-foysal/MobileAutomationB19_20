6bcaadc178c2d77449988c4fc4f541634e41bb29
arfoysal <arfoysal.me@gmail.com>
2023-11-10 23:45:59 +0600

    Add App install and remove. Add scrollIntoView and change selenium and appium version

--- src/test/java/org/example/RunGeneralStoreAPK.java
+++ src/test/java/org/example/RunGeneralStoreAPK.java
@@ -3,28 +3,43 @@ package org.example;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class RunGeneralStoreAPK {
+    public AndroidDriver driver;
 
-    @Test
-    public void runApp() throws MalformedURLException, InterruptedException {
+    @BeforeTest
+    public void startApp() throws MalformedURLException {
+        File f = new File("src/test/resources");
+        File fs = new File(f, "General-Store.apk");
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("platformName", "android");
         cap.setCapability("automationName", "UiAutomator2");
         cap.setCapability("deviceName", "local");
         cap.setCapability("udid", "emulator-5554");
-        cap.setCapability("appPackage","com.androidsample.generalstore");
-        cap.setCapability("appActivity","com.androidsample.generalstore.SplashActivity");
-        AndroidDriver driver = new AndroidDriver(new URL("http://172.20.10.5:4723/"), cap);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        cap.setCapability("app", fs.getAbsolutePath());
+//        cap.setCapability("appPackage","com.androidsample.generalstore");
+//        cap.setCapability("appActivity","com.androidsample.generalstore.SplashActivity");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), cap);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @Test
+    public void runApp(){
         driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.TextView[3]")).click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(text(\"Bangladesh\"));");
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Bangladesh']")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("TEST TEXT");
-        Thread.sleep(5000);
+    }
+    @AfterTest
+    public void removeApp(){
+        driver.removeApp("com.androidsample.generalstore");
     }
 }
